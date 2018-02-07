15469fd8247e68ed1841b04de49136c062420f5f
zcy <511929544@qq.com>
2018-02-07 18:00:36 +0800

    添加了注释

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	"dialect": "mysql",
 	"host": "localhost",
 	"port": 3306,
-	"database": "jd_db",
+	"database": "query",
 	"username": "root",
 	"password": "root",
 	"jwtSecret": "zcy",
