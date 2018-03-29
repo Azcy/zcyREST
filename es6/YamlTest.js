2d3136af89611699da345581d9f6b1d0746f8cf9
zcy <511929544@qq.com>
2018-03-29 16:31:06 +0800

    添加了YamlTest

---
+++ es6/YamlTest.js
@@ -0,0 +1,19 @@
+const YAML = require('yamljs');
+const fs = require("fs");
+// file为文件所在路径
+var data = YAML.parse(fs.readFileSync('../date.yaml').toString());
+//console.log(data);
+
+var host = data.paths;
+var paths = Object.keys(data.paths);
+var value=Object.values(paths)
+console.log(host);
+console.log(paths);
+console.log("value:"+value)
+
+
+var str = "123, 124, 234,252";
+var arr = str.split(",").map(function (val) {
+    return Number(val) + 1;
+});
+console.log(arr)
