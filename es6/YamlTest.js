0e303e4e87f22993d3a0acc1abefacf403f6a2fb
cy z <kowj728138@hotmail.com>
2018-03-29 23:45:25 +0800

    修改Data格式，添加yamljs

--- es6/YamlTest.js
+++ es6/YamlTest.js
@@ -1,21 +1,30 @@
-const yaml = require('js-yaml');
 const fs = require("fs");
+const YAML = require('yamljs');
+
 try {
-    var doc = yaml.safeLoad(fs.readFileSync('../date/GLOBAL.yaml', 'utf8'));
-    console.log(doc);
-} catch (e) {
+    var data = YAML.parse(fs.readFileSync('../date/Data.yaml').toString());
+    console.log(data);
+    var host = data.MODEL;
+    var paths = Object.keys(data.MODEL);
+    var value = Object.getOwnPropertyDescriptor(paths[0])
+
+    //console.log(host);
+    console.log(paths);
+    console.log(value)
+}catch (e) {
     console.log(e);
 }
 //var host = data.paths;
 //var paths = Object.keys(data.paths);
 //var value=Object.values(paths)
-//console.log(host);
-//console.log(paths);
-//console.log("value:"+value)
+/*console.log(host);
+console.log(paths);
+console.log("value:"+value)*/
 
 
+/*
 var str = "123, 124, 234,252";
 var arr = str.split(",").map(function (val) {
     return Number(val) + 1;
 });
-console.log(arr)
+console.log(arr)*/
